3e228f0dfd467bb046b1b3a6e6a7161578b9ac90
vindeshrajput <vindesh.rajput@gmail.com>
2024-07-15 20:27:25 +0530

    Update streamlit_app.py
    commented insert script

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
     time_to_insert = st.button('Submit_Order')
-    st.write(my_insert_stmt)
+    # st.write(my_insert_stmt)
     # st.stop()
     if time_to_insert:
        session.sql(my_insert_stmt).collect()
