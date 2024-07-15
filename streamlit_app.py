65ab163c2c328f2f73be1bf175e52ce76408e0de
vindeshrajput <vindesh.rajput@gmail.com>
2024-07-15 21:11:55 +0530

    indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -46,6 +46,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
+
 
     # st.write(ingredients_string)
 
@@ -59,10 +62,6 @@ if ingredients_list:
 	   
        st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
      
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())        
-fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
 
 
