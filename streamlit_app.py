269c864cc35ddd359b61ef89a98d77fdab472898
vindeshrajput <vindesh.rajput@gmail.com>
2024-07-15 17:12:44 +0530

    Update streamlit_app.py
    Changed session config

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -25,8 +26,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be: ',name_on_order)
 
 # Get the current credentials
+cnx = st.connection("snowflake")
 
-session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
