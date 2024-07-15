4cafa453b4ca1a3873e96d99a767c385890763a3
vindeshrajput <vindesh.rajput@gmail.com>
2024-07-15 22:37:22 +0530

    column fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ st.write('The name on your Smoothie will be: ',name_on_order)
 cnx = st.connection("snowflake")
 
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),select(col('SEARCH_ON')))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 ingredients_list = st.multiselect(
